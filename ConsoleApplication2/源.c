288f704f33508775f6e7770278f5de9a6b8005c1
LuoMing Xu <xjy46566696@163.com>
2017-03-04 23:45:55 +0800

    1-100猜数方案

--- ConsoleApplication2/源.c
+++ ConsoleApplication2/源.c
@@ -1,10 +1,34 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 int main(void)
 {
-	int number = 0;
+	int number, yournumber = 0;
+	int i = 0;
+	srand((unsigned)time(NULL));
+	for (i; i < 100; i++)
+	{
+		number = rand() % 101;
+		printf("%d\t", number);
+	}
+	printf("\n");
 
-	printf("请输入你所想到的1-100之间的数");
-	scanf_s("%d", &number);
+	printf("请输入你所认为的数");
+	scanf_s("%d", &yournumber);
 
-
+	while (number!=yournumber)
+	{
+		if (number > yournumber)
+		{
+			printf("小\n请重猜\n");
+			scanf_s("%d", &yournumber);
+			continue;
+		}	
+		else if (number < yournumber)
+		{
+			printf("大\n请重猜\n");
+			scanf_s("%d", &yournumber);
+		}
+	}
+	printf("恭喜你猜对了\n");
 }
