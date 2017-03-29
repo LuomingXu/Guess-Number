57d749ca6eb5a90ce1b158f8be9cc44b8830ac16
LuoMing Xu <xjy46566696@163.com>
2017-03-29 22:57:41 +0800

    张珂的快速排序

---
+++ 张珂的快速排序.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    int m, n, s;
+    std::string *names;
+    
+    std::cin >> m;
+    std::cin >> n >> s;
+    if (s > m)
+    {
+        std::cout << "Keep going..." << std::endl;
+        return 0;
+    }
+    names = new std::string[m];
+    for (int i = 0; i < m; ++i)
+        std::cin >> names[i];
+
+    std::vector<std::string> wins;
+    wins.reserve(m);
+
+    bool exist = false;
+    while (s <= m)
+    {
+        exist = false;
+        for (std::vector<std::string>::iterator beg = wins.begin(); beg != wins.end(); ++beg)
+        {
+            if (*beg == names[s - 1])
+            {
+                exist = true;  // exist and continue to check the next 
+                break;
+            }
+        }
+        if (!exist)
+        {
+            wins.push_back(names[s - 1]);
+            std::cout << names[s - 1] << std::endl;
+            s += n;
+        }
+        else
+            s += 1;
+    }
+    if (wins.size() == 0)
+        std::cout << "Keep going..." << std::endl;
+    return 0;
+}
